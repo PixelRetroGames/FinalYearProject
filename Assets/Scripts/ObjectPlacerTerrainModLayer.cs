9580d7ab6fa38b452355f6c17270a875a65725cc
Marian-Alexandru CIOLTAN <marian.cioltan@stud.acs.upb.ro>
2023-05-11 17:25:41 +0300

    Added perlin noise generation for splatmaps and trees

---
+++ Assets/Scripts/ObjectPlacerTerrainModLayer.cs
@@ -0,0 +1,154 @@
+using Cinemachine;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Terrain/Layers/ObjectPlacer")]
+[Serializable]
+public class ObjectPlacerTerrainModLayer : TerrainModLayer
+{
+    public float[,,] splatMap = null;
+
+    [Range(0.0f, 1.0f)]
+    public float noSpawnChance;
+
+    [System.Serializable]
+    public class TerrainLayerObjectsProps
+    {
+        public GameObject[] objects;
+        public int minDistance;
+        public int splatMapLayer;
+        public int[] indexes;
+    }
+
+    public TerrainLayerObjectsProps[] layers;
+
+    public void OnValidate()
+    {
+        Tool.OnValidate();
+    }
+
+    private void SetLayers()
+    {
+        List<TreePrototype> objects = new List<TreePrototype>();
+        int index = 0;
+        for (int i = 0; i < layers.Length; i++)
+        {
+            layers[i].indexes = new int[layers[i].objects.Length];
+            for (int j = 0; j < layers[i].objects.Length; j++)
+            {
+                var tree  = new TreePrototype();
+                tree.prefab = layers[i].objects[j];
+                tree.bendFactor = 0.2f;
+                tree.navMeshLod = 0; 
+                objects.Add(tree);
+                layers[i].indexes[j] = index;
+                index++;
+            }
+        }
+        Tool._TerrainData.treePrototypes = objects.ToArray();
+        Tool._TerrainData.RefreshPrototypes();
+    }
+
+    public override void Rebuild()
+    {
+        SetLayers();
+        //Tool._TerrainData.treeInstances = new TreeInstance[0];
+        Tool._TerrainData.SetTreeInstances(new TreeInstance[0], true);
+
+        var width = Tool._TerrainData.alphamapWidth;
+        var height = Tool._TerrainData.alphamapHeight;
+        splatMap = Tool._TerrainData.GetAlphamaps(0, 0, width, height);
+
+        for (int layer = 0; layer < layers.Length; layer++)
+        {
+            bool[,] placed = new bool[width, height];
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    float p = UnityEngine.Random.Range(0.0f, 1.0f);
+                    int splatMapLayer = layers[layer].splatMapLayer;
+                    if (p > splatMap[x, y, splatMapLayer] || splatMap[x, y, splatMapLayer] < 0.5 || p < noSpawnChance)
+                    {
+                        continue;
+                    }
+
+                    int l = layers[layer].minDistance;
+                    int minX = Math.Max(0, x - l);
+                    int minY = Math.Max(0, y - l);
+                    int maxX = Math.Min(width, x + l);
+                    int maxY = Math.Min(height, y + l);
+                    bool canSpawn = true;
+                    for (int i = minX; i < maxX && canSpawn; i++)
+                    {
+                        for (int j = minY; j < maxY && canSpawn; j++)
+                        {
+                            if (placed[i, j])
+                            {
+                                canSpawn = false;
+                            }
+                        }
+                    }
+
+                    if (!canSpawn)
+                    {
+                        continue;
+                    }
+
+                    Spawn(y, x, layer);
+                    placed[x, y] = true;
+                }
+            }
+
+                    /*float p = UnityEngine.Random.Range(0.0f, 1.0f);
+                    float sum = 0;
+                    int layerTypeToSpawn = 0;
+                    for (; layerTypeToSpawn < layerCount; layerTypeToSpawn++)
+                    {
+                        sum += splatMap[x, y, layerTypeToSpawn];
+                        if (p < sum)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (layerTypeToSpawn >= layerCount)
+                    {
+                        continue;
+                    }
+
+                    p = UnityEngine.Random.Range(0.0f, 1.0f);
+                    if (p < noSpawnChance)
+                    {
+                        continue;
+                    }
+                    Spawn(x, y, layerTypeToSpawn);*/
+        }
+
+        Tool._Terrain.Flush();
+    }
+
+    private void Spawn(float x, float y, int layer)
+    {
+        TreeInstance treeTemp = new TreeInstance();
+
+        var width = Tool._TerrainData.alphamapWidth;
+        var height = Tool._TerrainData.alphamapHeight;
+        treeTemp.position = new Vector3(x / width, 0, y / height);
+
+        treeTemp.prototypeIndex = layers[layer].indexes[UnityEngine.Random.Range(0, layers[layer].indexes.Length)];
+        treeTemp.widthScale = 1f;
+        treeTemp.heightScale = 1f;
+        //treeTemp.color = Color.white;
+        //treeTemp.lightmapColor = Color.white;
+        Tool._Terrain.AddTreeInstance(treeTemp);
+    }
+
+    public override void Apply()
+    {
+        Rebuild();
+    }
+}
---
+++ Assets/Scripts/TerrainTool.cs
@@ -0,0 +1,113 @@
+using UnityEngine;
+
+// SEE  for an explanation of forcegraph's initial implimentation
+// SEE https://youtu.be/6NT-OoEdWQE for an explanation 3ply perlin slopes.
+// SEE https://youtu.be/Vif8cdd-GVE for an explanation of 1 perlin slope combo.
+// SEE https://youtu.be/iRgh1EUxs6Y for an explanation of island borders.
+
+
+[ExecuteAlways]
+public class TerrainTool : MonoBehaviour
+{
+    public Terrain _Terrain;
+    public TerrainData _TerrainData;
+
+    public float[,] Mesh
+    {
+        get
+        {
+            return _Mesh;
+        }
+        set
+        {
+            _Mesh = value;
+        }
+    }
+
+    [System.Serializable]
+    public class LayerFields
+    {
+        public TerrainModLayer Layer;
+        public bool Rebuild = false;
+        public bool Apply = false;
+    }
+
+    public LayerFields[] Layers;
+    public bool RebuildAll = false;
+    public bool ApplyAll = false;
+
+    [HideInInspector]
+    public int HeightRes;
+    private float[,] _Mesh;
+    private Vector3 size;
+    private Vector3 scale;
+
+    public void OnEnable()
+    {
+        foreach (var layer in Layers)
+        {
+            layer.Layer.Tool = this;
+        }
+    }
+
+    public T GetLayer<T>() where T : TerrainModLayer
+    {
+        foreach (var layer in Layers)
+        {
+            if (layer.Layer is T) return (T)layer.Layer;
+        }
+
+        return null;
+    }
+
+    public void OnValidate()
+    {
+        RefreshTerrain();
+    }
+
+    public void RefreshTerrain()
+    {
+        HeightRes = _TerrainData.heightmapResolution;
+        size = _TerrainData.size;
+        scale = size / HeightRes;
+        Mesh = new float[HeightRes, HeightRes];
+
+        if (Layers != null)
+        {
+            foreach (var layer in Layers)
+            {
+                layer.Layer.Tool = this;
+                if (layer.Rebuild || RebuildAll)
+                {
+                    layer.Layer.Rebuild();
+                }
+
+                if (layer.Apply || ApplyAll)
+                {
+                    layer.Layer.Apply();
+                }
+            }
+        }
+
+        _TerrainData.SetHeights(0, 0, _Mesh);
+    }
+
+    public Vector3 WorldPositionFromHeightMapIndex(int x, int y)
+    {
+        return
+            new Vector3(
+                y * scale.z,
+                size.y * _Mesh[x, y],
+                x * scale.x)
+            + _Terrain.transform.position;
+    }
+
+    public float GetTerrainPercentForWorldHeight(float y)
+    {
+        if (y < _Terrain.transform.position.y) return 0;
+        if (y > _Terrain.transform.position.y + size.y) return 1;
+        y += 0 - _Terrain.transform.position.y;
+        return y / size.y;
+
+    }
+}
